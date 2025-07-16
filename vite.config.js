11c47390b75ce229a2649c196b8c21cb6ce1758f
이태영 <lhlhgodz3@gmail.com>
2025-07-16 23:34:52 +0900

    build: 빌드, 배포를 위한 파일 수정 및 404페이지 추가

--- vite.config.js
+++ vite.config.js
@@ -18,10 +18,16 @@ export default mergeConfig(
     },
     base,
     build: {
+      outDir: "dist",
+      assetsDir: "assets",
+      sourcemap: false,
       rollupOptions: {
         input: {
           main: resolve(__dirname, "index.html"),
           404: resolve(__dirname, "404.html"),
+        },
+        output: {
+          manualChunks: undefined,
         },
       },
     },
