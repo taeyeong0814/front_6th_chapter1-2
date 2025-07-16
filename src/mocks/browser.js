b14569331feeec2677dcfe2375741af30ec8eba9
이태영 <lhlhgodz3@gmail.com>
2025-07-17 00:07:25 +0900

    chore: ci 버전체크 코드 제거 및 MSW 설정

--- src/mocks/browser.js
+++ src/mocks/browser.js
@@ -1,5 +1,18 @@
 import { setupWorker } from "msw/browser";
-import { handlers } from "./handlers";
+import { handlers } from "./handlers.js";
+import { BASE_URL } from "../constants.js";
+
+export const worker = setupWorker(...handlers);
 
 // MSW 워커 설정
-export const worker = setupWorker(...handlers);
+// Worker start 옵션을 export하여 main.js에서 사용
+export const workerOptions = import.meta.env.PROD
+  ? {
+      serviceWorker: {
+        url: `${BASE_URL}/mockServiceWorker.js`,
+      },
+      onUnhandledRequest: "bypass",
+    }
+  : {
+      onUnhandledRequest: "bypass",
+    };
